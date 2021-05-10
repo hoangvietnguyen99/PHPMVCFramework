d7cda5536676d51f799140459d0685417247653d
hoangvietnguyen99 <hoangvietnguyen99@gmail.com>
2021-05-10 10:07:23 +0700

    Init commit

---
+++ core/Application.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Application
+ * @package app\core
+ */
+class Application
+{
+    public Request $request;
+    public Router $router;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+        $this->router = new Router($this->request);
+    }
+
+    public function run()
+    {
+        $this->router->resolve();
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vietnguyen/phpmvc-framework',
+  ),
+  'versions' => 
+  array (
+    'vietnguyen/phpmvc-framework' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
