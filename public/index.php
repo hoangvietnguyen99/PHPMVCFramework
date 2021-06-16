775e5df28c8fc5907b422f8540e4c2204f960026
Việt Nguyễn Hoàng <hoangvietnguyen99@gmail.com>
2021-06-16 20:49:19 +0700

    Add session, handle register form

--- public/index.php
+++ public/index.php
@@ -2,19 +2,28 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-use app\controllers\AuthController;
 use app\controllers\SiteController;
+use app\controllers\AboutController;
 use app\core\Application;
 
-$app = new Application(dirname(__DIR__));
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    "db" => [
+        "LOCAL" => $_ENV["DB_CONNECTION_STRING_LOCAL"],
+        "CLOUD" => $_ENV["DB_CONNECTION_STRING_CLOUD"],
+        "TYPE" => $_ENV["DB_SOURCE"]
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
 
 $app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/register', [SiteController::class, 'register']);
+$app->router->post('/register', [SiteController::class, 'register']);
+$app->router->get('/login', [SiteController::class, 'login']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
-
-$app->router->get('/login', [AuthController::class, 'login']);
-$app->router->post('/login', [AuthController::class, 'login']);
-
-$app->router->get('/register', [AuthController::class, 'register']);
-$app->router->post('/register', [AuthController::class, 'register']);
+$app->router->get('/about', [AboutController::class, 'index']);
 
 $app->run();
