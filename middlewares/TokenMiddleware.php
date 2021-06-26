8c264aeb3a5b62fc98bc4d289268899c7a62e5a4
Việt Nguyễn Hoàng <hoangvietnguyen99@gmail.com>
2021-06-26 16:52:17 +0700

    Fix toekn mdw

--- middlewares/TokenMiddleware.php
+++ middlewares/TokenMiddleware.php
@@ -17,15 +17,17 @@ class TokenMiddleware extends Middleware
      */
     public function execute()
     {
-        $request = Application::$application->request;
-        $response = Application::$application->response;
-        $token = $request->getBearerToken();
-        if (!$token) throw new UnauthorizedException(false);
-        $jwt = Application::$application->jwt;
-        try {
-            $jwt->validate($token);
-        } catch (Exception $exception) {
-            $response->send(401, ['message' => $exception->getMessage()]);
+        if (empty($this->actions) || in_array(Application::$application->controller->action, $this->actions)) {
+            $request = Application::$application->request;
+            $response = Application::$application->response;
+            $token = $request->getBearerToken();
+            if (!$token) throw new UnauthorizedException(false);
+            $jwt = Application::$application->jwt;
+            try {
+                $jwt->validate($token);
+            } catch (Exception $exception) {
+                $response->send(401, ['message' => $exception->getMessage()]);
+            }
         }
     }
 }
