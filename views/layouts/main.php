7afd47bf0c83a3986fe06b86c38ffeeb774bc3b2
levanminh10399 <leminh1031999@gmail.com>
2021-07-28 17:11:28 +0700

    - fit conlict in layout main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -51,79 +51,80 @@ License: You must have a valid license purchased only from themeforest(the above
 <!--begin::Body-->
 
 <body id="kt_body" class="header-fixed header-mobile-fixed page-loading">
-<!--begin::Main-->
-<!--begin::Header Mobile-->
-<div id="kt_header_mobile" class="header-mobile bg-primary header-mobile-fixed">
-    <!--begin::Logo-->
-    <a href="index.html">
-        <img alt="Logo" src="assets/media/logos/logo-1.png" class="max-h-30px" />
-    </a>
-    <!--end::Logo-->
-    <!--begin::Toolbar-->
-    <div class="d-flex align-items-center">
-        <button class="btn p-0 burger-icon burger-icon-left ml-4" id="kt_header_mobile_toggle">
-            <span></span>
-        </button>
-        <button class="btn p-0 ml-2" id="kt_header_mobile_topbar_toggle">
-					<span class="svg-icon svg-icon-xl">
-						<!--begin::Svg Icon | path:assets/media/svg/icons/General/User.svg-->
-						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-							<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-								<polygon points="0 0 24 0 24 24 0 24" />
-								<path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-								<path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
-							</g>
-						</svg>
-                        <!--end::Svg Icon-->
-					</span>
-        </button>
+    <!--begin::Main-->
+    <!--begin::Header Mobile-->
+    <div id="kt_header_mobile" class="header-mobile bg-primary header-mobile-fixed">
+        <!--begin::Logo-->
+        <a href="index.html">
+            <img alt="Logo" src="assets/media/logos/logo-letter-9.png" class="max-h-30px" />
+        </a>
+        <!--end::Logo-->
+        <!--begin::Toolbar-->
+        <div class="d-flex align-items-center">
+            <button class="btn p-0 burger-icon burger-icon-left ml-4" id="kt_header_mobile_toggle">
+                <span></span>
+            </button>
+            <button class="btn p-0 ml-2" id="kt_header_mobile_topbar_toggle">
+                <span class="svg-icon svg-icon-xl">
+                    <!--begin::Svg Icon | path:assets/media/svg/icons/General/User.svg-->
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <polygon points="0 0 24 0 24 24 0 24" />
+                            <path d="M12,11 C9.790861,11 8,9.209139 8,7 C8,4.790861 9.790861,3 12,3 C14.209139,3 16,4.790861 16,7 C16,9.209139 14.209139,11 12,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                            <path d="M3.00065168,20.1992055 C3.38825852,15.4265159 7.26191235,13 11.9833413,13 C16.7712164,13 20.7048837,15.2931929 20.9979143,20.2 C21.0095879,20.3954741 20.9979143,21 20.2466999,21 C16.541124,21 11.0347247,21 3.72750223,21 C3.47671215,21 2.97953825,20.45918 3.00065168,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
+                        </g>
+                    </svg>
+                    <!--end::Svg Icon-->
+                </span>
+            </button>
+        </div>
+        <!--end::Toolbar-->
     </div>
-    <!--end::Toolbar-->
-</div>
-<!--end::Header Mobile-->
-<div class="d-flex flex-column flex-root">
-    <!--begin::Page-->
-    <div class="d-flex flex-row flex-column-fluid page">
-        <!--begin::Wrapper-->
-        <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
-            <!--begin::Header-->
-            <div id="kt_header" class="header flex-column header-fixed">
-                <!--begin::Top-->
-                <div class="header-top">
-                    <!--begin::Container-->
-                    <div class="container">
-                        <!--begin::Left-->
-                        <div class="d-none d-lg-flex align-items-center mr-3">
-                            <!--begin::Logo-->
-                            <a href="/" class="mr-20">
-                                <img alt="Logo" src="assets/media/logos/logo-1.png" class="max-h-35px" />
-                            </a>
-                            <!--end::Logo-->
-                            <!--begin::Desktop Search-->
-                            <div class="quick-search quick-search-inline ml-4 w-300px" id="kt_quick_search_inline">
-                                <!--begin::Form-->
-                                <form method="get" class="quick-search-form">
-                                    <div class="input-group rounded bg-light">
-                                        <div class="input-group-prepend">
-													<span class="input-group-text">
-														<span class="svg-icon svg-icon-lg">
-															<!--begin::Svg Icon | path:assets/media/svg/icons/General/Search.svg-->
-															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																	<rect x="0" y="0" width="24" height="24" />
-																	<path d="M14.2928932,16.7071068 C13.9023689,16.3165825 13.9023689,15.6834175 14.2928932,15.2928932 C14.6834175,14.9023689 15.3165825,14.9023689 15.7071068,15.2928932 L19.7071068,19.2928932 C20.0976311,19.6834175 20.0976311,20.3165825 19.7071068,20.7071068 C19.3165825,21.0976311 18.6834175,21.0976311 18.2928932,20.7071068 L14.2928932,16.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
-																	<path d="M11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 Z M11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 C14.8659932,4 18,7.13400675 18,11 C18,14.8659932 14.8659932,18 11,18 Z" fill="#000000" fill-rule="nonzero" />
-																</g>
-															</svg>
-                                                            <!--end::Svg Icon-->
-														</span>
-													</span>
-                                        </div>
-                                        <input type="text" class="form-control h-45px" placeholder="Search..." />
-                                        <div class="input-group-append">
-													<span class="input-group-text">
-														<i class="quick-search-close ki ki-close icon-sm"></i>
-													</span>
+    <!--end::Header Mobile-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="d-flex flex-row flex-column-fluid page">
+            <!--begin::Wrapper-->
+            <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+                <!--begin::Header-->
+                <div id="kt_header" class="header flex-column header-fixed">
+                    <!--begin::Top-->
+                    <div class="header-top">
+                        <!--begin::Container-->
+                        <div class="container">
+                            <!--begin::Left-->
+                            <div class="d-none d-lg-flex align-items-center mr-3">
+                                <!--begin::Logo-->
+                                <a href="/" class="mr-20">
+                                    <img alt="Logo" src="assets/media/logos/logo-letter-9.png" class="max-h-35px" />
+                                </a>
+                                <!--end::Logo-->
+                                <!--begin::Desktop Search-->
+                                <div class="quick-search quick-search-inline ml-4 w-300px" id="kt_quick_search_inline">
+                                    <!--begin::Form-->
+                                    <form method="get" class="quick-search-form">
+                                        <div class="input-group rounded bg-light">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <span class="svg-icon svg-icon-lg">
+                                                        <!--begin::Svg Icon | path:assets/media/svg/icons/General/Search.svg-->
+                                                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                                <rect x="0" y="0" width="24" height="24" />
+                                                                <path d="M14.2928932,16.7071068 C13.9023689,16.3165825 13.9023689,15.6834175 14.2928932,15.2928932 C14.6834175,14.9023689 15.3165825,14.9023689 15.7071068,15.2928932 L19.7071068,19.2928932 C20.0976311,19.6834175 20.0976311,20.3165825 19.7071068,20.7071068 C19.3165825,21.0976311 18.6834175,21.0976311 18.2928932,20.7071068 L14.2928932,16.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                                                                <path d="M11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 Z M11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 C14.8659932,4 18,7.13400675 18,11 C18,14.8659932 14.8659932,18 11,18 Z" fill="#000000" fill-rule="nonzero" />
+                                                            </g>
+                                                        </svg>
+                                                        <!--end::Svg Icon-->
+                                                    </span>
+                                                </span>
+                                            </div>
+                                            <input type="text" class="form-control h-45px" placeholder="Search..." />
+                                            <div class="input-group-append">
+                                                <span class="input-group-text">
+                                                    <i class="quick-search-close ki ki-close icon-sm"></i>
+                                                </span>
+                                            </div>
                                         </div>
                                     </form>
                                     <!--end::Form-->
